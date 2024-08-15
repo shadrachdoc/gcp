ff1f223bee2077124c39176ebad375e51375742b
shadrachdoc <41693892+shadrachdoc@users.noreply.github.com>
2024-08-15 23:31:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,30 @@
 terraform {
   cloud {
-
     organization = "mypro"
-
     workspaces {
       name = "gcp"
     }
   }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"  # Use an appropriate version
+    }
+  }
 }
+
 provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "gke-cluster-test0815"
-  location = var.zone
-
+  name               = "gke-cluster-test0815"
+  location           = var.zone
   initial_node_count = 1
-
   node_config {
     machine_type = "e2-medium"
   }
 }
-
